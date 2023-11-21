c24341d0e8ec493fb6c588ed7d35061cc9a9491c
Harshit Tiwari <tiwariharshit679@gmail.com>
2023-11-21 10:57:45 -0800

    code

---
+++ src/main/java/com/example/makethetrip/flights.java
@@ -0,0 +1,16 @@
+package com.example.makethetrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class flights {
+    @GetMapping("/flights")
+    public String getData() {return " Please book your flight at 10% percent d at indigo";}
+}
+
+
+
+
+
+
---
+++ src/test/java/com/example/makethetrip/MakethetripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makethetrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakethetripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
