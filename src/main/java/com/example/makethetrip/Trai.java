3b575538f9ab64d905b950a53cc6f797e5cca8cb
Harshit Tiwari <tiwariharshit679@gmail.com>
2023-11-21 18:49:40 -0800

    f

--- src/main/java/com/example/makethetrip/Trai.java
+++ src/main/java/com/example/makethetrip/Trai.java
@@ -6,4 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Trai {
     @GetMapping("/Trai")
-    public String getData() {return " Please book ticket from irctc";}
+    public String getData() {
+        return " Please book ticket from irctc";
+    }
+}
